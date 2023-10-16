ef59c85a9baaf4f4ef00dc3bdc4a060fa3a40f7b
Agung Nawawi <agung@fairtech.com.sg>
2023-10-16 10:35:35 +0700

    Initial Commit

---
+++ Snack/SnackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnackApp.swift
+//  Snack
+//
+//  Created by Agung Nawawi on 16/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SnackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
