b60c16b3fdf60c2d62de357823438b8595090491
Agung Nawawi <agung@fairtech.com.sg>
2023-10-16 21:52:35 +0700

    Create navigation view on category

---
+++ Snack/Views/CategoryView.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryView.swift
+//  Snack
+//
+//  Created by Agung Nawawi on 16/10/23.
+//
+
+import SwiftUI
+
+struct CategoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryView()
+}
